1ce65a86952bb48b9b2fbf29a5781b4f2b93a5e4
jenpoyueh <jenpoyueh@gmail.com>
2019-09-17 14:53:01 +0800

    修bug

--- games/games006/js/game.js
+++ games/games006/js/game.js
@@ -323,7 +323,7 @@ game.State.play={
     },
     preload:function(){
         //虛擬手把圖片
-        this.load.atlas('arcade', '../games006/assets/arcade-joystick.png', '/games/games006/assets/arcade-joystick.json');
+        this.load.atlas('arcade', '../games006/assets/arcade-joystick.png', '../games/games006/assets/arcade-joystick.json');
     },
     create:function(){
         //分數
